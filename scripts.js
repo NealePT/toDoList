a2371d2a1e902dec92cfcb8d14b2d86f323a5bfc
Neale Taylor <nealeptaylor@gmail.com>
2022-10-18 11:42:58 -0400

    complete script functionality

--- scripts.js
+++ scripts.js
@@ -1,26 +1,68 @@
-const newTask = document.getElementById("newTaskInput");
-const addTaskButton = document.getElementById("addTaskButton");
-const taskList = document.getElementsByClassName("taskList");
-const taskTemplate = document.getElementsByClassName("taskTemplate");
+const newTask = document.querySelector('#newTaskInput');
+const addTaskBtn = document.querySelector('.addTaskButton');
+const removeCompleteBtn = document.querySelector('.removeCompleteButton');
+const taskList = document.querySelector('.taskList');
+const taskTemplate = document.querySelector('#taskTemplate');
 let id = 1;
 
-let taskData = JSON.parse(localStorage.getItem('taskData')) || [];
+let taskData = [];
 
 const addTask = (data) => {
   const taskElement = document.importNode(taskTemplate.content, true);
-  const checkbox = taskElement.querySelector("input");
+  const checkbox = taskElement.querySelector('input');
   checkbox.id = id;
-  const label = taskElement.querySelector("label");
+  const label = taskElement.querySelector('label');
   label.htmlFor = id;
   label.append(data.description);
   taskList.appendChild(taskElement);
-  newTask.value = "";
+  newTask.value = '';
   id += 1;
 };
 
+const inputValid = () => {
+  return newTask.value !== '';
+};
+
 const renderData = () => {
-  taskData.forEach((data) => {
-    addTask(data);
-  });
+  taskData.forEach((data) => addTask(data));
 };
 renderData();
+
+newTask.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13 && inputValid()) {
+    const data = {
+      description: newTask.value,
+      completed: false,
+      index: id,
+    };
+    addTask(data);
+    taskData.push(data);
+  }
+});
+
+addTaskBtn.addEventListener('click', () => {
+  if (inputValid()) {
+    const data = {
+      description: newTask.value,
+      completed: false,
+      index: id,
+    };
+    addTask(data);
+    taskData.push(data);
+  }
+});
+
+removeCompleteBtn.addEventListener('click', () => {
+  const tasks = document.querySelectorAll('.task');
+  const deleted = [];
+  tasks.forEach((task) => {
+    const { checked } = task.querySelector('input');
+    if (checked) {
+      deleted.push(Number(task.querySelector('label').htmlFor));
+      task.remove();
+    }
+  });
+  taskData = taskData.filter((data) => {
+    return !deleted.includes(data.index);
+  });
+});
